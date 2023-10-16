20a87b7ef1b293de779d0874b9623bc34f72fec2
newObjectccc <897333949@qq.com>
2023-10-16 16:32:27 +0800

    feat(kch-backend-api-gateway): api代理转发设置

--- packages/kch-backend-api-gateway/src/main.ts
+++ packages/kch-backend-api-gateway/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { AppModule } from './app.module';
+require('dotenv').config({
+  path: `./.env.${process.env.NODE_ENV ?? 'production'}`
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  //createProxyMiddleware
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: `http://127.0.0.1:${process.env.TARGET_SERVER_EXPOSE_PORT ?? 3000}`,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    })
+  );
+
+  await app.listen(process.env.SERVER_EXPOSE_PORT ?? 443);
 }
 bootstrap();
