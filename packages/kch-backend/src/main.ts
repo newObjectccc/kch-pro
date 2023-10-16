f234bc838f2430b3d9e955e4bfff66b0745b436f
newObjectccc <897333949@qq.com>
2023-10-16 11:12:37 +0800

    feat(kch-backend): swagger配置

--- packages/kch-backend/src/main.ts
+++ packages/kch-backend/src/main.ts
@@ -1,11 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 require('dotenv').config({ path: `./.env.${process.env.NODE_ENV ?? 'production'}` });
 
 async function bootstrap() {
+  // app instance
   const app = await NestFactory.create(AppModule);
+
+  // global pipe
   app.useGlobalPipes(new ValidationPipe());
+
+  // swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Swagger Documentation')
+    .setDescription('Apis Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
+  // app run
   await app.listen(process.env.SERVER_EXPOSE_PORT ?? 3000);
 }
 bootstrap();
