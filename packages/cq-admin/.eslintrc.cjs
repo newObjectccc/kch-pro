2919243f53231ee344e5958df10b349125558be8
newObjectccc <897333949@qq.com>
2023-10-18 17:34:40 +0800

    chore(utils): eslintrc修改

--- packages/cq-admin/.eslintrc.cjs
+++ packages/cq-admin/.eslintrc.cjs
@@ -2,12 +2,11 @@ module.exports = {
   extends: [
     'react-app',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    // 'plugin:react/jsx-runtime'
+    'plugin:react-hooks/recommended'
   ],
   rules: {
     // 可按需添加或修改规则
-    "react/react-in-jsx-scope": 0,
-    "react/display-name": 0
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0
   }
 };
