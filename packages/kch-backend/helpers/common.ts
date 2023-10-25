59ac92c91c3483b578b22c19a3cfdebf5d5619c7
newObjectccc <897333949@qq.com>
2023-10-25 18:02:32 +0800

    style(kch-backend): 删掉空行

--- packages/kch-backend/helpers/common.ts
+++ packages/kch-backend/helpers/common.ts
@@ -2,16 +2,13 @@ import * as bcrypt from 'bcrypt';
 
 export async function hashPassword(password: string) {
   let result = null;
-
   const salt = await bcrypt.genSalt(10);
   result = await bcrypt.hash(password, salt);
-
   return result;
 }
 
 export async function comparePassword(password: string, hash: string) {
   let result = null;
-
   result = await bcrypt.compare(password, hash);
   return result;
 }
