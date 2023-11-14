ca2ab41d8745d0a53a37a10ed05698b04eeec14f
guanxuerong <guanxuerong@chabaidao.com>
2023-11-14 16:54:13 +0800

    feat:tabbar-icon

---
+++ packages/kch-miniapp/src/pages/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="login-container"> {{ name }} </view>
+</template>
+
+<script setup lang="ts">
+const name = '登录';
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  font-size: 15rpx;
+}
+</style>
