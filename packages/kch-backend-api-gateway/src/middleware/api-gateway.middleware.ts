d0f9f01bbcd6aef08200b5d34817b683665436a7
newObjectccc <897333949@qq.com>
2023-11-24 16:06:28 +0800

    feat(kch-backend-api-gateway): 转发api及其他服务

--- packages/kch-backend-api-gateway/src/middleware/api-gateway.middleware.ts
+++ packages/kch-backend-api-gateway/src/middleware/api-gateway.middleware.ts
@@ -9,7 +9,7 @@ import SIGN_API from '../utils/signApi';
 export class ProxyMiddleware implements NestMiddleware {
   constructor(private authService: AuthService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    const proxyUrl = `http://127.0.0.1:${process.env.API_SERVER_EXPOSE_PORT ?? 3000}`;
+    let proxyUrl = `http://127.0.0.1:${process.env.API_SERVER_EXPOSE_PORT ?? 3000}`;
     const proxyOptions = {
       proxyReqPathResolver: (req) => req.originalUrl.replace('/api', ''), // 设置代理请求路径
       proxyReqBodyDecorator: async (bodyContent, srcReq) => {
@@ -29,10 +29,14 @@ export class ProxyMiddleware implements NestMiddleware {
         return modifyBody;
       }
     };
+    let proxyMiddleware = null;
     console.log(req.url);
     if (!/^\/api/.test(req.url)) {
+      proxyUrl = `http://127.0.0.1:${process.env.FRONTEND_SERVER_EXPOSE_PORT ?? 8080}`;
+      proxyMiddleware = proxy(proxyUrl);
+    } else {
+      proxyMiddleware = proxy(proxyUrl, proxyOptions);
     }
-    const proxyMiddleware = proxy(proxyUrl, proxyOptions);
 
     proxyMiddleware(req, res, next);
   }
