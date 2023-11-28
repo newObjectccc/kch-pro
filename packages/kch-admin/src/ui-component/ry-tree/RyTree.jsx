9cb1fe80d9a97495ee98f8c2332fcbd565510322
newObjectccc <897333949@qq.com>
2023-11-28 16:25:42 +0800

    feat(kch-miniapp): ryTree组件开发及分类页面开发

--- packages/kch-admin/src/ui-component/ry-tree/RyTree.jsx
+++ packages/kch-admin/src/ui-component/ry-tree/RyTree.jsx
@@ -6,53 +6,167 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { isValidArray } from 'utils/commonCheck';
+
+const StyledListItemIcon = (props) =>
+  React.createElement(ListItemIcon, { sx: { alignItems: 'center' }, ...props });
+
+const StyledCollapse = (props) =>
+  React.createElement(Collapse, { sx: { pl: (theme) => theme.spacing(3.5) }, ...props });
+
+const StyledExpandMore = (props) =>
+  React.createElement(ExpandMore, { sx: { ml: (theme) => theme.spacing(1) }, ...props });
+
+const StyledExpandLess = (props) =>
+  React.createElement(ExpandLess, { sx: { ml: (theme) => theme.spacing(1) }, ...props });
+
+const StyledListItemButton = (props) =>
+  React.createElement(ListItemButton, { sx: { width: '300px', position: 'relative' }, ...props });
+
+const StyledList = styled(List)(({ theme }) => ({
+  '& .MuiList-root': {
+    padding: '0'
+  }
+}));
+
+const EditSVG = ({ onClick }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ padding: '0 4px' }}
+      stroke="#9E9E9E"
+      className="icon icon-tabler icon-tabler-edit"
+      width="32"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={onClick}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+      <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+      <path d="M16 5l3 3" />
+    </svg>
+  );
+};
+EditSVG.propTypes = {
+  onClick: PropTypes.func
+};
+
+const TrashSVG = ({ onClick }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-trash"
+      width="32"
+      height="24"
+      style={{ padding: '0 4px' }}
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="#9E9E9E"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={onClick}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 7l16 0" />
+      <path d="M10 11l0 6" />
+      <path d="M14 11l0 6" />
+      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+    </svg>
+  );
+};
+TrashSVG.propTypes = {
+  onClick: PropTypes.func
+};
 
 // RyTree
 function RyTree(props) {
-  const { value, listProps, listItemProps, onChange, isChecked, children } = props;
+  const {
+    value,
+    listProps,
+    listItemProps,
+    onChange,
+    checked,
+    onRemove,
+    onEdit,
+    rowAction,
+    children
+  } = props;
 
   if (typeof children === 'function') {
     return children(value);
   }
 
-  const clickHandler = (val) => {
-    if (val.children?.length > 0) val.isCollapsed = !val.isCollapsed;
-    onChange?.(value);
+  const clickHandler = (type, val, final = true) => {
+    if (type === 'collapse' && val.children?.length > 0) val.isCollapsed = !val.isCollapsed;
+    if (type === 'check') {
+      val.isChecked = !val.isChecked;
+      if (val.children?.length > 0)
+        val.children.map((child) => clickHandler('check', child, false));
+    }
+    final && onChange?.(value);
   };
 
   // recursion render
   const renderHandler = (item, listItemProps, listProps) => {
     const isValidArr = isValidArray(item?.children);
     if (isValidArr) item.isCollapsed = item.isCollapsed || false;
+    if (checked) item.isChecked = item.isChecked || false;
     return (
       <div key={item.id}>
-        <ListItemButton {...listItemProps} onClick={() => clickHandler(item)}>
-          <ListItemIcon>
-            {isChecked ? <Checkbox checked={item.isChecked}></Checkbox> : null}
+        <StyledListItemButton {...listItemProps}>
+          <StyledListItemIcon>
+            {checked ? (
+              <Checkbox
+                checked={item.isChecked}
+                onChange={() => clickHandler('check', item)}
+              ></Checkbox>
+            ) : null}
             <ListItemText primary={item.name}></ListItemText>
-            {item.isCollapsed === true ? <ExpandMore /> : null}
-            {item.isCollapsed === false ? <ExpandLess /> : null}
-          </ListItemIcon>
-        </ListItemButton>
+          </StyledListItemIcon>
+          <div onClick={() => clickHandler('collapse', item)}>
+            {item.isCollapsed === true ? <StyledExpandMore /> : null}
+            {item.isCollapsed === false ? <StyledExpandLess /> : null}
+          </div>
+          {rowAction ? (
+            <div style={{ right: '0', position: 'absolute' }}>
+              {typeof rowAction === 'function' ? (
+                rowAction(item)
+              ) : (
+                <>
+                  <EditSVG onClick={() => onEdit?.(item)} />
+                  <TrashSVG onClick={() => onRemove?.(item)} />
+                </>
+              )}
+            </div>
+          ) : null}
+        </StyledListItemButton>
         {isValidArr ? (
-          <Collapse sx={{ paddingLeft: '16px' }} in={item.isCollapsed}>
+          <StyledCollapse in={item.isCollapsed}>
             <List {...listProps}>
               {item.children.map((i) => {
                 return renderHandler(i, listItemProps, listProps);
               })}
             </List>
-          </Collapse>
+          </StyledCollapse>
         ) : null}
       </div>
     );
   };
 
   return (
-    <List sx={{ width: '400px' }} {...listProps}>
+    <StyledList {...listProps}>
       {Array.isArray(value) && value.map((item) => renderHandler(item, listItemProps, listProps))}
-    </List>
+    </StyledList>
   );
 }
 
@@ -62,7 +176,10 @@ RyTree.propTypes = {
   listProps: PropTypes.object,
   listItemProps: PropTypes.object,
   onChange: PropTypes.func,
-  isChecked: PropTypes.bool
+  onRemove: PropTypes.func,
+  onEdit: PropTypes.func,
+  checked: PropTypes.bool,
+  rowAction: PropTypes.oneOfType([PropTypes.bool, PropTypes.func])
 };
 
 export default RyTree;
--- packages/kch-admin/src/views/pages/category/CategoryMain.js
+++ packages/kch-admin/src/views/pages/category/CategoryMain.js
@@ -14,7 +14,18 @@ const mockData = [
           {
             id: 4,
             name: '马斯洛心理测评',
-            children: []
+            children: [
+              {
+                id: 6,
+                name: '犯罪倾向测评',
+                children: []
+              },
+              {
+                id: 7,
+                name: '自杀倾向测评',
+                children: []
+              }
+            ]
           }
         ]
       }
@@ -35,10 +46,20 @@ const mockData = [
 
 const CategoryManagement = (props) => {
   const [value, setValue] = useState(mockData);
-  const onChangeHandler = (val) => setValue([...val]);
+  const onChangeHandler = (val) => {
+    setValue([...val]);
+  };
+
   return (
     <MainCard title="分类管理">
-      <RyTree value={value} onChange={onChangeHandler}></RyTree>
+      <RyTree
+        value={value}
+        checked
+        rowAction
+        onRemove={console.log}
+        onEdit={console.log}
+        onChange={onChangeHandler}
+      ></RyTree>
     </MainCard>
   );
 };
