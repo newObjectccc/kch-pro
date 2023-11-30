14f7cd83df6daa09aba27a764e34181557f6c96f
newObjectccc <897333949@qq.com>
2023-11-30 14:38:09 +0800

    fix(kch-backend-api-gateway): 网关部署修改

--- packages/kch-backend-api-gateway/src/middleware/api-gateway.middleware.ts
+++ packages/kch-backend-api-gateway/src/middleware/api-gateway.middleware.ts
@@ -5,12 +5,11 @@ import { AuthService } from 'src/auth/auth.service';
 import NO_VERIFY_API from '../utils/noVerifyApi';
 import SIGN_API from '../utils/signApi';
 
-const serviceUrl = process.env.NODE_ENV !== 'production' ? 'localhost' : 'nginx';
 @Injectable()
 export class ProxyMiddleware implements NestMiddleware {
   constructor(private authService: AuthService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    let proxyUrl = `http://${serviceUrl}:${process.env.API_SERVER_EXPOSE_PORT ?? 3000}`; // 特别注意一下这里，cqbackend 是 docker-compose.yml 部署的 service 的服务名，需要docker network中需要这样去访问才可以
+    let proxyUrl = `http://${process.env.API_HOST}:${process.env.API_SERVER_EXPOSE_PORT ?? 81}`;
     const proxyOptions = {
       proxyReqPathResolver: (req) => req.originalUrl.replace('/api', ''), // 设置代理请求路径
       proxyReqBodyDecorator: async (bodyContent, srcReq) => {
