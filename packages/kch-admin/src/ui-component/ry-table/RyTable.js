d1e262b4af16dc4035bb4ade89199369ead36cd2
newObjectccc <897333949@qq.com>
2023-12-13 17:02:33 +0800

    fix(RyTable): fix default value

--- packages/kch-admin/src/ui-component/ry-table/RyTable.js
+++ packages/kch-admin/src/ui-component/ry-table/RyTable.js
@@ -68,25 +68,25 @@ const Empty = (columns, createFunc) => {
   return (
     <TableBody>
       <tr>
-      <td colSpan={columns.length}>
-        <StyledEmpty>
-          <div style={{ width: '100%' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mood-empty" width="60" height="60" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-              <path d="M9 10l.01 0" />
-              <path d="M15 10l.01 0" />
-              <path d="M9 15l6 0" />
-            </svg>
-            <div className='emptyText'>暂无数据</div>
-            <div>
-              {createFunc && <RyButton variant="text" onClick={() => handleClick()}>
-                去添加
-              </RyButton>}
+        <td colSpan={columns.length}>
+          <StyledEmpty>
+            <div style={{ width: '100%' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mood-empty" width="60" height="60" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                <path d="M9 10l.01 0" />
+                <path d="M15 10l.01 0" />
+                <path d="M9 15l6 0" />
+              </svg>
+              <div className='emptyText'>暂无数据</div>
+              <div>
+                {createFunc && <RyButton variant="text" onClick={() => handleClick()}>
+                  去添加
+                </RyButton>}
+              </div>
             </div>
-          </div>
-        </StyledEmpty>
-      </td>
+          </StyledEmpty>
+        </td>
       </tr>
     </TableBody>
   )
@@ -103,8 +103,7 @@ PaginationComponent.propTypes = {
   pagination: PropTypes.object
 };
 
-const RyTable = ({ columns, dataSource, pagination }) => {
-
+const RyTable = ({ columns = [], dataSource = [], pagination }) => {
   if (!Array.isArray(columns) || columns?.length < 1)
     return console.warn('RyTable columns must be an array!');
 
