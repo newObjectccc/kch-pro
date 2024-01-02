17a7a52d331a8d76460c14c5a36223ab9d9f7f3f
newObjectccc <897333949@qq.com>
2024-01-02 08:24:45 +0800

    chore(kch-backend-api-gateway): 添加ssl协议,csrf和dos防御

--- packages/kch-backend-api-gateway/src/app.module.ts
+++ packages/kch-backend-api-gateway/src/app.module.ts
@@ -1,11 +1,20 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from 'src/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyMiddleware } from './middleware/api-gateway.middleware';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 30
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- packages/kch-backend-api-gateway/src/main.ts
+++ packages/kch-backend-api-gateway/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { HttpExceptionFilter } from 'src/filter/http-exception.filter';
 import { AppModule } from './app.module';
+const fs = require('fs');
 require('dotenv').config({
   path: `./.env.${process.env.NODE_ENV ?? 'production'}`
 });
 
+const httpsOptions = {
+  key: fs.readFileSync('~/etc/rysyclub.com.key'),
+  cert: fs.readFileSync('~/etc/rysyclub.com_bundle.crt')
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true, bodyParser: false });
+  const app = await NestFactory.create(AppModule, { cors: true, bodyParser: false, httpsOptions });
+  app.use(helmet());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.SERVER_EXPOSE_PORT ?? 80);
 }
